f3c29e67e25d64c8516cab707ce58126da76e8b6
Elie Mugenzi <eliemugenzi@Elies-MacBook-Pro.local>
2019-10-13 18:52:24 +0200

    Made a carousel of post images

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,59 +1,22 @@
 <template>
-  <div>
-    <h3>Home</h3>
-    <ApolloQuery :query="getPostsQuery">
-      <!-- <template slot-scope="{result:{ loading, error, data, networkStatus }}"> -->
-       <template slot-scope="{result:{ loading, error, data }}">
-         <div v-if="loading">Loading...</div>
-         <div v-else-if="error">{{error}}</div>
-         <!-- <div v-else-if="networkStatus">Network status: {{networkStatus}} </div> -->
-         <ul v-else v-for="post in data.getPosts" :key="post._id">
-           <li>
-             {{post.title}}
-             {{post.description}}
-             {{post.likes}}
-
-           </li>
-         </ul>
-       </template>
-    </ApolloQuery>
-  </div>
+  <v-container text-xs-center v-if="getPosts">
+      <v-flex xs12>
+         <v-carousel v-bind="{'clycle':true}" interval="3000">
+            <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+               <h1 id="carousel__title">{{post.title}}</h1>
+            </v-carousel-item>
+         </v-carousel>
+      </v-flex>
+  </v-container>
 </template>
 
 <script>
 import { gql } from "apollo-boost";
 export default {
   name: "home",
-  // apollo: {
-  //   getPosts: {
-  //     query: gql`
-  //       {
-  //         getPosts {
-  //           _id
-  //           title
-  //           imageUrl
-  //           description
-  //           likes
-  //         }
-  //       }
-  //     `,
-
-  //     // Alternative way of getting the results from a query
-  //     result({ data, loading, networkStatus }) {
-  //       if (!loading) {
-  //         this.posts = data.getPosts;
-  //         console.log("Network status", networkStatus);
-  //       }
-  //     },
-  //     error(err) {
-  //       console.error(err);
-  //     }
-  //   }
-  // },
-  data() {
-    return {
-      posts: [],
-      getPostsQuery:gql`
+  apollo: {
+    getPosts: {
+      query: gql`
         {
           getPosts {
             _id
@@ -61,10 +24,41 @@ export default {
             imageUrl
             description
             likes
+            imageUrl
           }
         }
-      `
+      `,
+
+      // Alternative way of getting the results from a query
+      result({ data, loading, networkStatus }) {
+        if (!loading) {
+          this.posts = data.getPosts;
+          console.log("Network status", networkStatus);
+        }
+      },
+      error(err) {
+        console.error(err);
+      }
+    }
+  },
+  data() {
+    return {
+      posts: []
     };
   }
 };
 </script>
+
+<style lang="scss" scoped>
+   #carousel__title{
+     position: absolute;
+     background:rgba(0,0,0,.5);
+     color:white;
+     border-radius: 5px 5px 0 0;
+     padding: 0.5em;
+     margin:0 auto;
+     bottom: 50px;
+     left: 0;
+     right: 0;
+   }
+</style>
