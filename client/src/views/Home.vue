5942d2183e98044ca0fdc19fb80c3a6b5e098ffb
Elie Mugenzi <eliemugenzi@Elies-MacBook-Pro.local>
2019-10-13 18:39:15 +0200

    Another way of making a request inside a template part

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
     <h3>Home</h3>
-    <div v-if="$apollo.loading">Loading...</div>
-    <ul
-      v-else
-      v-for="post in getPosts"
-      :key="post._id"
-    >
-      <li>
-        {{post.title}} {{post.description}}
-      </li>
-      <li>{{post.likes}} likes</li>
-    </ul>
+    <ApolloQuery :query="getPostsQuery">
+      <!-- <template slot-scope="{result:{ loading, error, data, networkStatus }}"> -->
+       <template slot-scope="{result:{ loading, error, data }}">
+         <div v-if="loading">Loading...</div>
+         <div v-else-if="error">{{error}}</div>
+         <!-- <div v-else-if="networkStatus">Network status: {{networkStatus}} </div> -->
+         <ul v-else v-for="post in data.getPosts" :key="post._id">
+           <li>
+             {{post.title}}
+             {{post.description}}
+             {{post.likes}}
+
+           </li>
+         </ul>
+       </template>
+    </ApolloQuery>
   </div>
 </template>
 
@@ -19,9 +24,36 @@
 import { gql } from "apollo-boost";
 export default {
   name: "home",
-  apollo: {
-    getPosts: {
-      query: gql`
+  // apollo: {
+  //   getPosts: {
+  //     query: gql`
+  //       {
+  //         getPosts {
+  //           _id
+  //           title
+  //           imageUrl
+  //           description
+  //           likes
+  //         }
+  //       }
+  //     `,
+
+  //     // Alternative way of getting the results from a query
+  //     result({ data, loading, networkStatus }) {
+  //       if (!loading) {
+  //         this.posts = data.getPosts;
+  //         console.log("Network status", networkStatus);
+  //       }
+  //     },
+  //     error(err) {
+  //       console.error(err);
+  //     }
+  //   }
+  // },
+  data() {
+    return {
+      posts: [],
+      getPostsQuery:gql`
         {
           getPosts {
             _id
@@ -31,23 +63,7 @@ export default {
             likes
           }
         }
-      `,
-
-      // Alternative way of getting the results from a query
-      result({ data, loading, networkStatus }) {
-        if (!loading) {
-          this.posts = data.getPosts;
-          console.log("Network status", networkStatus);
-        }
-      },
-      error(err) {
-        console.error(err);
-      }
-    }
-  },
-  data() {
-    return {
-      posts: []
+      `
     };
   }
 };
